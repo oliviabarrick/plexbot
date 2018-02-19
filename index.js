4110ccf07277d79dd280393e1268dd584dbbbf73
Your Name <you@example.com>
2018-02-19 02:23:30 +0000

    improve oauth

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ var events = 'direct_mention,direct_message';
 
 prometheus.collectDefaultMetrics({ timeout: 5000 });
 
-var controller = Botkit.slackbot({});
+var controller = Botkit.slackbot({
+    json_file_store: './db_slackbutton_bot/',
+}).configureSlackApp({
+    clientId: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    redirectUri: process.env.SLACK_APP_URL,
+    scopes: ['bot'],
+});
 
 controller.setupWebserver(8889, function(err, express_webserver) {
     controller.createWebhookEndpoints(express_webserver)
+    controller.createOauthEndpoints(express_webserver, function(err, req, res) {
+        if (err) {
+            res.status(500).send('ERROR: ' + err);
+        } else {
+            res.send('Success!');
+        }
+    });
 
     var metrics_callback = function(req, res) {
         res.set('Content-Type', prometheus.register.contentType);
@@ -21,14 +35,20 @@ controller.setupWebserver(8889, function(err, express_webserver) {
     express_webserver.get('/metrics', metrics_callback);
 });
 
-var bot = controller.spawn({
-    token: process.env.SLACK_TOKEN
+controller.on('create_bot', function(bot, config) {
+    bot.startRTM(function(err) {});
 });
 
-bot.startRTM(function(err, bot, payload) {
-    if(err) {
-        throw err;
-    }
+controller.hears(triggers, events, search.searchHandler);
 
-    controller.hears(triggers, events, search.searchHandler);
+controller.storage.teams.all(function(err, teams) {
+    for (var t in teams) {
+        if (teams[t].bot) {
+            controller.spawn(teams[t]).startRTM(function(err, bot) {
+                if (err) {
+                    console.log('Error connecting bot to Slack:',err);
+                }
+            });
+       }
+    }
 });
