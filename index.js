6d1f5ec086bfd5e3fc5a204766b7711f2f7307c0
Your Name <you@example.com>
2018-02-19 01:08:29 +0000

    add prometheus

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var Botkit = require('botkit');
 var search = require('./search');
+var prometheus = require('prom-client');
 
 var triggers = ['^add (tv|movie) (.*)$'];
 var events = 'direct_mention,direct_message';
+
+prometheus.collectDefaultMetrics({ timeout: 5000 });
 
 var controller = Botkit.slackbot({});
 
 controller.setupWebserver(8889, function(err, express_webserver) {
     controller.createWebhookEndpoints(express_webserver)
+
+    var metrics_callback = function(req, res) {
+        res.set('Content-Type', prometheus.register.contentType);
+        res.end(prometheus.register.metrics());
+    };
+
+    express_webserver.get('/', metrics_callback);
+    express_webserver.get('/metrics', metrics_callback);
 });
 
 var bot = controller.spawn({
