fcec8903a6e9e8c2a515a68b4cc0466bd1cf1460
Your Name <you@example.com>
2018-02-24 08:22:07 +0000

    fix triggers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Botkit = require('botkit')
 var search = require('./search')
 var prometheus = require('prom-client')
 
-var triggers = ['^add (tv|movie|radarr) (.*)$']
+var triggers = ['^add (tv|movie|couchpotato) (.*)$']
 var events = 'direct_mention,direct_message'
 
 prometheus.collectDefaultMetrics({ timeout: 5000 })
